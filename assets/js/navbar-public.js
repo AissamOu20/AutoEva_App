eea1637b7bb436b20e7c766638c1f7de70bbdcb8
Aissam <mac@192.168.1.63>
2025-10-28 12:19:05 +0100

    fix CSP meta position

--- assets/js/navbar-public.js
+++ assets/js/navbar-public.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const container = document.getElementById("navbar");
-  const response = await fetch("/components/navbar-public.html");
-  container.innerHTML = await response.text();
+  
+  // Make sure this path is correct relative to your index.html
+  const response = await fetch("./components/navbar-public.html"); 
+  
+  if (response.ok) {
+    container.innerHTML = await response.text();
+  } else {
+    console.error("Failed to load navbar component:", response.status);
+  }
 });
